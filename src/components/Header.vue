3c9c952d19396507b905fc793b9f1990e20c50dd
lindabettini <linda.bettini@hotmail.it>
2022-02-21 12:55:03 +0100

    add flex in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,18 @@
 <template>
-	<header>
-		<div class="container">
-			<div class="row">
-				<nav class="col-12">
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Services</a></li>
-						<li><a href="#">Why Us</a></li>
-						<li><a href="#">Case Studies</a></li>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">(555) 802-1234</a></li>
-						<li><a href="#">Free Quote</a></li>
-					</ul>
-				</nav>
-			</div>
+	<header class="container">
+		<div class="row justify-content-center align-items-center">
+			<nav class="col-12 d-flex justify-content-center align-items-center">
+				<ul class="d-flex justify-content-center align-items-center">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Why Us</a></li>
+					<li><a href="#">Case Studies</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">(555) 802-1234</a></li>
+					<li><a href="#">Free Quote</a></li>
+				</ul>
+			</nav>
 		</div>
 	</header>
 </template>
@@ -32,13 +30,20 @@ export default {
 @import "@/scss/style.scss";
 header {
 	height: 80px;
-	ul {
-		display: inline-flex;
-		list-style-type: none;
-		li a {
-			color: black;
-			text-decoration: none;
-			padding: 15px;
+	.row {
+		height: 100%;
+		nav {
+			height: 100%;
+			ul {
+				height: 100%;
+				display: inline-flex;
+				list-style-type: none;
+				li a {
+					color: black;
+					text-decoration: none;
+					padding: 15px;
+				}
+			}
 		}
 	}
 }
