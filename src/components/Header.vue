dc360f4c29476a44c0ff683917c5848a700b6534
lindabettini <linda.bettini@hotmail.it>
2022-02-21 12:38:11 +0100

    add nav ul

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,22 @@
 <template>
-	<header>ciao</header>
+	<header>
+		<div class="container">
+			<div class="row">
+				<nav class="col-12">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Why Us</a></li>
+						<li><a href="#">Case Studies</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">(555) 802-1234</a></li>
+						<li><a href="#">Free Quote</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
 </template>
 
 <script>
@@ -14,6 +31,15 @@ export default {
 <style scoped lang="scss">
 @import "@/scss/style.scss";
 header {
-	color: $main-color;
+	height: 80px;
+	ul {
+		display: inline-flex;
+		list-style-type: none;
+		li a {
+			color: black;
+			text-decoration: none;
+			padding: 15px;
+		}
+	}
 }
 </style>
