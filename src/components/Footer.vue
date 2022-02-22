bb9bff734f120abd227762c26aeb3503fb4289d7
lindabettini <linda.bettini@hotmail.it>
2022-02-22 18:27:34 +0100

    add cols in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,35 @@
 <template>
-	<footer></footer>
+	<footer>
+		<div class="row">
+			<div class="col-6"></div>
+			<div class="col-2" v-for="(menu, index) in menus" :key="index">
+				<h6>{{ menu.title }}</h6>
+				<ul>
+					<li v-for="(item, index) in menu.items" :key="index">{{ item }}</li>
+				</ul>
+			</div>
+			<div class="col-2"></div>
+			<div class="col-2"></div>
+		</div>
+	</footer>
 </template>
 
 <script>
 export default {
 	name: "Footer",
-	props: {
-		msg: String,
+	data() {
+		return {
+			menus: [
+				{
+					title: "Services",
+					items: ["uno", "due", "tre"],
+				},
+				{
+					title: "Services",
+					items: ["uno", "due", "tre"],
+				},
+			],
+		};
 	},
 };
 </script>
